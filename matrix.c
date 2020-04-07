92d399fd1415bc36ec314155d650776275aca3cb
sh4nnu <manikishanghantasala@gmail.com>
2020-04-08 02:04:40 +0530

    Modify matrix.c to give page reclaims, page faults, and TAT

--- matrix.c
+++ matrix.c
@@ -1,54 +1,104 @@
 // C program to multiply two square matrices.
 #include <stdio.h>
 #include <sys/time.h>
-#define N 4
+#include <sys/resource.h>
+#include <stdlib.h>
+#include <string.h>
+
 
 // This function multiplies mat1[][] and mat2[][],
 // and stores the result in res[][]
-void multiply(int mat1[][N], int mat2[][N], int res[][N])
+void multiply(int** mat1, int** mat2, int** res, int n, int  m, int k)
 {
-    int i, j, k;
-    for (i = 0; i < N; i++)
-    {
-        for (j = 0; j < N; j++)
-        {
-            res[i][j] = 0;
-            for (k = 0; k < N; k++)
-                res[i][j] += mat1[i][k]*mat2[k][j];
-        }
+   int i,j,c;
+
+    for (i = 0; i < n; i++)
+   {
+    for (j = 0; j < k; j++)
+      {
+           res[i][j] = 0;
+	  
+            for (c = 0; c < m; c++){
+
+                res[i][j] += mat1[i][c]*mat2[c][j];
+	    }
+      }
+
+
     }
+//    for (i = 0; i < n; i++)
+//    {
+//        for (j = 0; j < k; j++)
+//           printf("%d ", res[i][j]);
+//       printf("\n");
+//    }
+ 
+}
+void matrix_mult(int n, int m, int k)
+{
+    int i,j;
+    int** mat1;
+     mat1 = malloc(n * sizeof *mat1);
+    for (i=0; i<n; i++)
+    {
+	mat1[i] = malloc(m * sizeof *mat1[i]);
+    }
+
+   for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < m; j++)
+           mat1[i][j] = rand()%1000;
+    }
+ 
+     int** mat2;
+    
+      mat2 = malloc(m * sizeof *mat2);
+    for (i=0; i<m; i++)
+    {
+	mat2[i] = malloc(k * sizeof *mat2[i]);
+    }
+   
+   for (i = 0; i < m; i++)
+    {
+        for (j = 0; j < k; j++)
+           mat2[i][j] = rand()%1000;
+    }
+ 
+    int** res; // To store result
+    res =  malloc(n  * sizeof *res);
+
+   for (i = 0; i < n; i++)
+    {
+        res[i] = malloc(k * sizeof *res[i]); 
+    }
+ 
+    multiply(mat1, mat2, res,n,m,k);
+//    printf("first matrix is \n");
+//   printf("Result matrix is \n");
+//    for (i = 0; i < n; i++)
+//    {
+//        for (j = 0; j < k; j++)
+//           printf("%d ", res[i][j]);
+//        printf("\n");
+//    }
+    
 }
 
-int main()
-{
-struct timeval  tv1, tv2;
-gettimeofday(&tv1, NULL);
-
-    int mat1[N][N] = { {1, 1, 1, 1},
-                    {2, 2, 2, 2},
-                    {3, 3, 3, 3},
-                    {4, 4, 4, 4}};
-
-    int mat2[N][N] = { {1, 1, 1, 1},
-                    {2, 2, 2, 2},
-                    {3, 3, 3, 3},
-                    {4, 4, 4, 4}};
-
-    int res[N][N]; // To store result
-    int i, j;
-    multiply(mat1, mat2, res);
-
-    printf("Result matrix is \n");
-    for (i = 0; i < N; i++)
-    {
-        for (j = 0; j < N; j++)
-           printf("%d ", res[i][j]);
-        printf("\n");
-    }
-gettimeofday(&tv2, NULL);
-
-printf ("Total time = %f seconds\n",
-         (double) (tv2.tv_usec - tv1.tv_usec) / 1000000 +
-         (double) (tv2.tv_sec - tv1.tv_sec));
-    return 0;
+int main(int argc, char** argv) {
+	struct rusage buf;
+	int n,m,k;
+	n = atoi(argv[1]);
+	m = atoi(argv[2]);
+	k = atoi(argv[3]);
+	matrix_mult(n,m,k);
+	getrusage(RUSAGE_SELF, &buf);
+//	printf("Page reclaims:  %ld\n", buf.ru_minflt);
+//	printf("Page faults:    %ld\n", buf.ru_majflt); 
+//	printf("user time in mircoseconds: %e\n",(double) buf.ru_utime.tv_sec 	 + (double) buf.ru_utime.tv_usec / (double) 1000000); 
+//	printf("system time in mircoseconds: %e\n",(double) buf.ru_stime.tv_sec + (double) buf.ru_stime.tv_usec / (double) 1000000); 
+// the out put is page reclaims, page faults, tat.	
+	printf("%ld,%ld,%e\n",buf.ru_minflt, buf.ru_majflt,
+		       	((double) buf.ru_utime.tv_sec + 
+			(double) buf.ru_utime.tv_usec / (double) 1000000) +( (double) buf.ru_stime.tv_sec + 
+			(double) buf.ru_stime.tv_usec / (double) 1000000));
 }
